e1f6e71f9bf0bec0c5c99bb8b9a5439fde9e3465
Ishrat Jaben Bushra <83410344+ishrat1102@users.noreply.github.com>
2022-02-16 20:20:12 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\SingleAction;
-use App\Http\Controllers\ResourcePhoto;
 use App\Http\Controllers\TestFormController;
 
 
@@ -18,40 +15,8 @@ use App\Http\Controllers\TestFormController;
 |
 */
 
-//Route::get('/', function () {
-//   return view('welcome');
-//});
-/*Route::get('/{name?}', function ($name = null) {
-    $demo = "<h2>will execute and not print html only in between text of tags</h2>";
-    $data = compact('name', 'demo');
 
-    return view('learnB')->with($data);
-});*/
-
-/*Route::get('/', function () {
-
-    return view('home');
-});
-Route::get('/about', function () {
-
-    return view('about');
-});*/
-/*Route::get('/test/{name}/{id?}', function ($name, $id = null) {
-    //echo $name;
-    //echo $id;
-    $data = compact('name', 'id');
-    //print_r($data);
-    return view('test')->with($data);
-});*/
-//using controller to return view'home'
-Route::get('/', [TestController::class, 'home']);
-//Route::get('/about', [TestController::class, 'about']);
-Route::get('/about', 'App\Http\Controllers\TestController@about'); //not good approach using this way
-//use of single action
-Route::get('/course', SingleAction::class);
-//use of resource
-Route::resource('/photo', ResourcePhoto::class);
 //submit form
 Route::get('/register', [TestFormController::class, 'index']);
 Route::post('/register', [TestFormController::class, 'view_form']);
-//to see errors use $errors variable
+
